c03e19e07345c44c22fbf4cb375c168c68670ff1
optimizacija <males.jan2@gmail.com>
2021-05-13 18:16:10 +0200

    wip: 1st working solution for angular

--- lua/lsp/angular.lua
+++ lua/lsp/angular.lua
@@ -1,7 +1,20 @@
+-- TODO: it's WIP, but it's working *woop woop*
 -- :LspInstall anoular
-require'lspconfig'.angularls.setup {
-    cmd = {DATA_PATH .. "/lspinstall/angular/node_modules/@angular/language-server/bin/ngserver", "--stdio"},
+local cmd = {
+    DATA_PATH .. "/lspinstall/angular/node_modules/@angular/language-server/bin/ngserver", "--stdio",
+    '--tsProbeLocations', '/Users/jan/.nvm/versions/node/v12.18.2/lib/node_modules/',
+    '--ngProbeLocations', '/Users/jan/.nvm/versions/node/v12.18.2/lib/node_modules/',
+    }
+
+require'lspconfig'.angularls.setup{
+    cmd = cmd,
+    on_new_config = function(new_config, new_root_dir)
+        new_config.cmd = cmd
+    end,
     filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx" },
     root_dir = require('lspconfig/util').root_pattern("angular.json", ".git"),
     on_attach = require'lsp'.on_attach,
 }
+
+-- TODO: debug
+print(DATA_PATH .. "/lspinstall/angular/node_modules/@angular/language-server/bin/ngserver")
